57314cc8a7b66182ad083c9048125bd2b7ea911c
NikitaSavc <micraftur@gmail.com>
2024-05-29 17:31:48 +0300

    common: Add conditions for bengal & bengal-5.15
    * We have platform bengal-5.15 (with 5.15 kernel version) with same
      TARGET_BOARD_PLATOFRM (bengal) as bengal with 4.19 kernel version.
      Because of this, platforms conflict, and we should distinguish between
      these platforms
    * Bengal-5.15 has TARGET_BOARD_SUFFIX equal _515, which is specific.
    
    Change-Id: Ib3cf80c348face6eac8552c15859d1700dd60659
    Signed-off-by: NikitaSavc <micraftur@gmail.com>

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -7,7 +7,14 @@ UM_3_18_FAMILY := msm8996 msm8937 msm8953
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := msmnile $(MSMSTEPPE) $(TRINKET) atoll
-UM_4_19_FAMILY := kona lito bengal
+UM_4_19_FAMILY := kona lito
 UM_5_4_FAMILY := lahaina holi
 UM_5_10_FAMILY := taro parrot
 UM_5_15_FAMILY := kalama crow
+
+# Conditions for bengal & bengal_515
+ifneq ($(filter _515,$(TARGET_BOARD_SUFFIX)),)
+   UM_5_15_FAMILY += bengal
+else
+   UM_4_19_FAMILY += bengal
+endif
